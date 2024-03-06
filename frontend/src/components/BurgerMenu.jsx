a7d6ee7a9b18c49df12d3c6dbe8d0196a0a6386d
Suzy-cas <contact@scassar.com>
2024-03-06 15:41:08 +0100

    add picto to menus

--- frontend/src/components/BurgerMenu.jsx
+++ frontend/src/components/BurgerMenu.jsx
@@ -2,6 +2,11 @@
 import { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import varietyPic from "../assets/picto/Picto_potato_quest_white.svg";
+import recipePic from "../assets/picto/Picto_recipe_white.svg";
+import profilePic from "../assets/picto/Picto_profile_white.svg";
+import registerPic from "../assets/picto/Picto_add_white.svg";
+import homePic from "../assets/picto/Picto_home_white.svg";
 
 function BurgerMenu() {
   const [showBurger, setShowBurger] = useState(true);
@@ -24,16 +29,35 @@ function BurgerMenu() {
         <span className="burger-bar" />
       </button>
       <ul className={`burger-ul${showBurger ? "hidden" : ""}`}>
-        <Link to="/">Accueil</Link>
-        <Link to="/varietes">Variétés</Link>
-        <Link to="/recettes">Recettes</Link>
+        <Link className="link" to="/">
+          <img src={homePic} alt="" />
+          <h1>Accueil</h1>
+        </Link>
+        <Link className="link" to="/varietes">
+          <img src={varietyPic} alt="" />
+          <h1>Variétés</h1>
+        </Link>
+        <Link className="link" to="/recettes">
+          <img src={recipePic} alt="" />
+          <h1>Recettes</h1>
+        </Link>
         {user.is_admin === 3 ? (
-          <Link to="/inscription">Inscription</Link>
+          <Link className="link" to="/inscription">
+            <img src={registerPic} alt="" /> <h1>Inscription</h1>
+          </Link>
         ) : (
-          <Link to="/mon-espace/profil">Mon espace</Link>
+          <Link className="link" to="/mon-espace/profil">
+            {" "}
+            <img src={profilePic} alt="" />
+            <h1>Mon espace</h1>
+          </Link>
         )}
-        <Link to={user.is_admin === 3 ? "/connexion" : "/deconnexion"}>
-          {user.is_admin === 3 ? "Connexion" : "Deconnexion"}
+        <Link
+          className="link"
+          to={user.is_admin === 3 ? "/connexion" : "/deconnexion"}
+        >
+          <img src={profilePic} alt="" />
+          {user.is_admin === 3 ? <h1>Connexion</h1> : <h1>Deconnexion</h1>}
         </Link>
       </ul>
     </nav>
