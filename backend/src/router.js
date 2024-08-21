2b4c973e8d844526f6f49bc45a477a0b8a00e916
Suzy-cas <contact@scassar.com>
2024-08-21 10:25:56 +0200

    modified router for upload and moved uploaded files

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,27 +1,27 @@
 /* eslint-disable camelcase */
 const express = require("express");
 
-const router = express.Router();
-
 const path = require("path");
+
+const router = express.Router();
 
 const multer = require("multer");
 
-const uploadAvatar = multer({
-  dest: "public/uploads/avatars",
-  fileFilter: (_req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png/;
-    const mimetype = fileTypes.test(file.mimetype);
-    const extname = fileTypes.test(path.extname(file.originalname));
-    if (mimetype && extname) {
-      return cb(null, true);
-    }
-    cb(
-      `Error: File upload only supports the following filetypes - ${fileTypes}`
-    );
-    return "";
-  },
-});
+// const uploadAvatar = multer({
+//   dest: "public/uploads/avatars",
+//   fileFilter: (_req, file, cb) => {
+//     const fileTypes = /jpeg|jpg|png/;
+//     const mimetype = fileTypes.test(file.mimetype);
+//     const extname = fileTypes.test(path.extname(file.originalname));
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     }
+//     cb(
+//       `Error: File upload only supports the following filetypes - ${fileTypes}`
+//     );
+//     return "";
+//   },
+// });
 const uploadRecipeImage = multer({
   dest: "public/uploads/recipes",
   fileFilter: (_req, file, cb) => {
@@ -148,12 +148,13 @@ router.post(
   uploadRecipeImage.single("recipePic"),
   uploads.uploadRecipePictures
 );
-router.post(
-  "/uploads/avatars/:id",
-  uploadAvatar.single("avatar"),
-  uploads.uploadAvatars
-);
-router.put("/users/edit-avatar/:id", userControllers.editAvatar);
+// router.post(
+//   "/uploads/avatars/:id",
+//   uploadAvatar.single("avatar"),
+//   uploads.uploadAvatars
+// );
+// router.put("/users/edit-avatar/:id", userControllers.editAvatar);
+
 // ROLE WALL
 
 module.exports = router;
